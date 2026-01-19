498b059298af384e7d33f792303fda646ad5e3a6
Rron Grezda <rrongrezda5@gmail.com>
2026-01-19 21:27:20 +0200

    Removed commented excercises.

--- 11-funksionet.py
+++ 11-funksionet.py
@@ -1,54 +1,50 @@
-# def say_hello():
-#     print("Hello Rron!")
+def say_hello():
+    print("Hello Rron!")
 
-# say_hello()
+say_hello()
 
+print("---------------------")
 
+db_username = "Rron Grezda"
+db_password = "1234"
 
-# db_username = "Rron Grezda"
-# db_password = "1234"
+def login():
+    username = input("Shkruaj username-in tend: ")
+    password = input("Shkruaj password: ")
 
-# def login():
-#     username = input("Shkruaj username-in tend: ")
-#     password = input("Shkruaj password: ")
+    if username == db_username and password == db_password:
+            print("Home page")
+    else:
+        print("Wrong username or password!")
 
-#     if username == db_username and password == db_password:
-#             print("Home page")
-#     else:
-#         print("Wrong username or password!")
+login()
 
-# login()
+print("---------------------")
 
+def pershendtje(username):
+    return "Hello " + username
 
-
-# def pershendtje(username):
-#     return "Hello " + username
-
-# username = input("Shkruaj username tend: ")
-# print(pershendtje(username))
-
-
-
-# def say_hello(username):
-#     print("Hello "+ username)
-
-# username = input("Shkruaj username tend: ")
-# say_hello(username)
+username = input("Shkruaj username tend: ")
+print(pershendtje(username))
 
 
+print("---------------------")
 
-# def greet(first_name, last_name):
-#     print(f"Hello {first_name} {last_name}")
-#     print("Welcome on board!")
+def greet(first_name, last_name):
+    print(f"Hello {first_name} {last_name}")
+    print("Welcome on board!")
 
-# greet("Rron", "Grezda")
-# greet("Albina", "Grezda")
+greet("Rron", "Grezda")
+greet("Albina", "Grezda")
 
-# def get_name(name):
-#     print("Hello", name)
+print("---------------------")
 
-# get_name("Rron")
+def get_name(name):
+    print("Hello", name)
 
+get_name("Rron")
+
+print("---------------------")
 
 def even_or_odd(number):
     if number % 2 == 0:
