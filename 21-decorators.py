1380e282c01b46f757e8ca54b50f1cf29fcb424c
Rron Grezda <rrongrezda5@gmail.com>
2026-01-24 18:15:41 +0200

    Multiple decorator

--- 21-decorators.py
+++ 21-decorators.py
@@ -8,3 +8,22 @@ def decorated_function():
     return "This is a decorated text"
 
 print(decorated_function())
+
+print("--------------------")
+
+# Multiple decorations
+def multiple_decorators(func):
+    def decorate():
+        return func().upper()
+    return decorate
+
+@multiple_decorators
+def first_dec_func():
+    return "First decorated function."
+
+@multiple_decorators
+def sec_dec_func():
+    return "Second decorated function."
+
+print(first_dec_func())
+print(sec_dec_func())
