79bb2f032a09a7c2a8e6a05b23915bb8ef6af777
Rron Grezda <rrongrezda5@gmail.com>
2026-02-03 18:41:26 +0200

    Tuples

--- 23-tuples.py
+++ 23-tuples.py
@@ -4,4 +4,14 @@ print(seasons[1])
 
 for s in seasons:
     print(s)
+
+if "Winter" in seasons:
+    print("Cold!")
+else:
+    print("Not so cold!")
+
+if "X season" in seasons:
+    print("Hot!")
+else:
+    print("Not so hot!")
 #seasons.append("X season")           -Error: 'tuple' object has no attribute 'append'
