1dcf757b46643a9410ffb40c7a8c3e6fb8c650a2
Rron Grezda <rrongrezda5@gmail.com>
2026-02-03 18:37:39 +0200

    Iterate through tuples

--- 23-tuples.py
+++ 23-tuples.py
@@ -1,4 +1,7 @@
 seasons = ("Spring", "Summer", "Autumn", "Winter")
 
 print(seasons[1])
+
+for s in seasons:
+    print(s)
 #seasons.append("X season")           -Error: 'tuple' object has no attribute 'append'
