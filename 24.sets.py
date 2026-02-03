57593b4f570af6281be6120dc11d4af754f6012f
Rron Grezda <rrongrezda5@gmail.com>
2026-02-03 19:01:19 +0200

    Sets

---
+++ 24.sets.py
@@ -0,0 +1,9 @@
+'''
+Sets in Python are unordered collections of unique elements (no duplicates).
+We use them to remove duplicates, check membership quickly, and perform set operations like union, intersection, and difference. { }
+'''
+
+group_members = {"Rron", "Oscar", "Tim", "Oscar"}
+
+for member in group_members:
+    print(member)
