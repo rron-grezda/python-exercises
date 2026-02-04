5c95f4f87c1202e9ed9e7b4fb8c5bf61fc996a92
Rron Grezda <rrongrezda5@gmail.com>
2026-02-04 18:50:44 +0200

    Dicts

---
+++ 25.dicts.py
@@ -0,0 +1,12 @@
+'''
+In Python, dictionaries (dicts) are a built-in data type used to store data in key-value pairs.
+'''
+students = {
+    # key: value
+    "name": "Rron",
+    "surname": "Grezda",
+    "age": "24"
+}
+
+print(students["name"])
+print(students["name"] +" "+ students["surname"] +" "+ str(students["age"]))
