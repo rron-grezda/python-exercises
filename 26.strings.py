5c5a27e68955aa3079c7b24fb4275a17a7d55710
Rron Grezda <rrongrezda5@gmail.com>
2026-02-05 19:06:54 +0200

    Strip function

--- 26.strings.py
+++ 26.strings.py
@@ -1,7 +1,7 @@
 '''
 In Python, a STRING is a data type used to store text.
 '''
-string = ("Python is a high-level, interpreted, general-purpose programming language known for its simple, English-like syntax and readability, making it ideal for beginners and rapid development.")
+string = (" python is a high-level, interpreted, general-purpose programming language known for its simple, English-like syntax and readability, making it ideal for beginners and rapid development.    ")
 
 for s in string:
     print(s)
@@ -19,3 +19,8 @@ print(string.endswith("development."))
 print(string.count("for"))
 print(string.split(" "))
 print(type(string.split(" ")))
+print(string.title())
+print(string.capitalize())
+print(string.strip()) # Removes spaces at the beginning and at the end
+print(string.lstrip()) # Removes spaces at the left
+print(string.rstrip()) # Removes spaces at the right
