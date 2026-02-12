e321f6f440ed25f447ee2bf8b993c4568cb5d134
Rron Grezda <rrongrezda5@gmail.com>
2026-02-12 16:51:37 +0200

    Remove a fruit from the list

--- 27.error-handling.py
+++ 27.error-handling.py
@@ -25,9 +25,22 @@ print("-------------------------------------")
 numbers = [10, 20, 30]
 
 try:
-    index = int(input("Enter an index (0, 1, 2)"))
+    index = int(input("Enter an index (0, 1, 2): "))
     print("The value of the entered index is: ", numbers[index])
 except IndexError:
     print("Index does not exist!")
 except ValueError:
     print("Please enter a valid number.")
+
+print("-------------------------------------")
+
+fruits = ["apple", "orange", "lemon", "blueberry", "grape"]
+print(fruits)
+try:
+    
+    fruit = input("Enter a fruit to remove: ")
+    fruits.remove(fruit)
+    print("The fruit was removed successfully.")
+    print("Updated list: ", fruits)
+except ValueError:
+    print("This fruit does not exist in the list.")
