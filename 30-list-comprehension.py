bf351253f33b25bce87b00730558549e0a6d88e7
Rron Grezda <rrongrezda5@gmail.com>
2026-02-15 17:20:01 +0200

    Excercises with List comprehensions

--- 30-list-comprehension.py
+++ 30-list-comprehension.py
@@ -18,3 +18,14 @@ numbers = [n for n in range(1, 21)]
 new_list = [n for n in numbers if n % 6 == 0]
 
 print(new_list)
+
+# Create a list with the squares of numbers from 1 to 10.
+square = [n**2 for n in range(1, 11)]
+print(square)
+
+# From the list: ⬇️ create a new list where each number is multiplied by 2.
+
+list_numbers = [5, 8, 12, 3, 7]
+
+multiply_list = [n*2 for n in list_numbers]
+print(multiply_list)
