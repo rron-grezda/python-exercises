fb5d0e3737e9837be3fb3c750e00e353704e6fbc
Rron Grezda <rrongrezda5@gmail.com>
2026-03-13 20:00:41 +0200

    Excercises

---
+++ 31.ushtrime.py
@@ -0,0 +1,8 @@
+# name = input("What is your name? ")
+# print("Hello",name,"welcome!")
+
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+
+result = num1 + num2
+print("The result is: ", result)
